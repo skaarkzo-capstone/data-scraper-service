7d6b4d501506829c8e7a24387ceebe6f86e84008
Sufyan Khan <skhan943@uwo.ca>
2024-11-14 20:27:45 -0500

    Remove comment

--- app/core/config.py
+++ app/core/config.py
@@ -4,6 +4,5 @@ class Settings(BaseSettings):
     APP_NAME: str = "SustAIn Data Scraper"
     VERSION: str = "1.0.0"
     API_PREFIX: str = "/api/scraper"
-    # Will add more setting (e.g., DATABASE_URL)
 
 settings = Settings()
